efeed120ebbcd21c1bb846d4099d617cb9b79bed
VDucDung <dunga3qv2@gmail.com>
2024-04-15 10:38:07 +0700

    ➕ Config env variable

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,1 +1,10 @@
+import 'dotenv/config'
 
+export const env = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  MONGDB_NAME: process.env.MONGDB_NAME || 'trello-db',
+  APP_HOST: process.env.APP_HOST || 'localhost',
+  APP_PORT: process.env.APP_PORT || 3000,
+
+  AUTHOR: process.env.AUTHER || 'vuducdung'
+}
