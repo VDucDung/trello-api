8d574486619d0a228c052e82cc6ac594d2f9941b
VDucDung <dunga3qv2@gmail.com>
2024-04-19 00:05:02 +0700

    👍Implement error handling

--- src/server.js
+++ src/server.js
@@ -5,12 +5,13 @@ import exitHook from 'async-exit-hook'
 import { env } from '~/config/environment'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { APIs_v1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
   app.use(express.json())
   app.use('/v1', APIs_v1)
-
+  app.use(errorHandlingMiddleware)
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Server running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
@@ -21,7 +22,7 @@ const START_SERVER = () => {
 }
 
 CONNECT_DB()
-  .then(() => { console.log('Connected to database!'); })
+  .then(() => { console.log('Connected to database!') })
   .then(() => { START_SERVER() })
   .catch(err => {
     console.log(err)
